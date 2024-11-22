d9b6c6b5ee108245bfbc0dc4a45afaab6e67b238
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2024-11-22 21:58:52 +0700

    add links to github and linkedin and remove instagram

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import logo from "../assets/Logo.png";
-import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -8,9 +8,22 @@ const Navbar = () => {
         <img src={logo} alt="logo" className="w-10 mx-2" />
       </div>
       <div className="flex items-center gap-4 m-8 text-2xl">
-        <FaLinkedin className="cursor-pointer hover:text-gray-400" />
-        <FaGithub className="cursor-pointer hover:text-gray-400" />
-        <FaInstagram className="cursor-pointer hover:text-gray-400" />
+        <a
+          href="https://linkedin.com/in/aung-htet-paing-4482622b2"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn Profile"
+        >
+          <FaLinkedin className="cursor-pointer hover:text-gray-400" />
+        </a>
+        <a
+          href="https://github.com/aunghtetpaing10"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub Profile"
+        >
+          <FaGithub className="cursor-pointer hover:text-gray-400" />
+        </a>
       </div>
     </nav>
   );
